23dbcb33991648b3bf144b8883bb76c9ebc3a552
Henry Choi <4735067+fluxt@users.noreply.github.com>
2020-10-17 17:31:08 -0500

    added Flask hello world and renamed routes to support multiple of them

--- api/api.py
+++ api/api.py
@@ -3,6 +3,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/time')
+@app.route('/')
+def hello_world():
+    return "Hello World from Flask"
+
+@app.route('/api/time')
 def get_current_time():
     return {'time': time.time()}
