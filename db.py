02d60e09aca5cc5ad74a9d61bdabd0f1c1f29ba7
Henry Choi <4735067+fluxt@users.noreply.github.com>
2020-12-02 10:51:29 -0600

    forgot to remove one more file

--- db.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask 
-from flask_pymongo import pymongo
-from application import application
-
-CONNECTION_STRING = 'mongodb+srv://user:SQLPassword@flask-mongodb.rlb9i.mongodb.net/Forum?retryWrites=true&w=majority'
-
-client = pymongo.MongoClient(CONNECTION_STRING)
-db = client.get_database('Forum')
-user_collection = pymongo.collection.Collection(db, 'Discussion')
